3bd3c5416451e8f341b3c0e7596da95c3a9198a2
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-12-10 13:07:11 -0500

    further indexedDB psuedocode

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -2,15 +2,26 @@
 
 //declare db and request to open db
 
+ //onupgradeneeded (schema)
  //On install, create bulk (pending) collection and save to indexDB
 
 //activate (onsuccess)
     //check if app online before reading from db
+    //checkDatabase (if online)
+
+//on error
 
  //saveRecord (save to index db incase install fails)
  function saveRecord(data) {
     console.log(data);
        
 }
+
+//checkDatabase, ref db + store
+    //getAll onsuccess
+    //if something in bulk, going to post it (bc back online)
+
+    //.then (turn response into json)
+    //.then delete records if successful
 
 //listen to when back online and save records
