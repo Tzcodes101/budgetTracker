5769eeaf84374b60b3ce623ed451bb6bc71570c9
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-12-10 12:50:52 -0500

    redo indexDB

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,2 +1,14 @@
-//svaeRecord function that console logs data
-    //check for service worker 
+function saveRecord(data) {
+    //check for service worker
+
+
+    //SW lifecycle
+
+        //On install, create bulk (pending) collection and save to indexDB
+
+        //activate
+
+        //saveRecord (save to index db incase install fails)
+
+        //listen to when back online and send records 
+}
