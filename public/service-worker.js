3eae8acc25ffcefc574cf19faeae0035c4062e70
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-12-10 22:45:34 -0500

    files to cache added to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,15 @@
 const CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const FILES_TO_CACHE = {
+const FILES_TO_CACHE = [
     //add files to cache
-}
+    "./indexedDB.js",
+    "./index.html",
+    "./styles.css",
+    "./index.js",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png"
+]
 
 //service worker code
 self.addEventListener('install', (event) => {
