23f6ab9fc74d399b1dcac92eafa36daa21e56305
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-12-10 22:40:26 -0500

    service-worker.js activate and fetch code added

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,6 +16,42 @@ self.addEventListener('install', (event) => {
 });
 
 //clean up old caches
+self.addEventListener(`activate`, event => {
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames =>
+                // return array of cache names that are old to delete
+                cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
+            )
+            .then(cachesToDelete =>
+                Promise.all(
+                    cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete))
+                )
+            )
+            .then(() => self.clients.claim())
+    );
+});
 
 
 //fetch from caches
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+  
+          return caches.open(DATA_CACHE_NAME).then((cache) => {
+            return fetch(event.request).then((response) => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    }
+  });
