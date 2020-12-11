09f6f1ca19fb3498af5b952643fd3aee26e91700
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-12-10 22:37:26 -0500

    install service-worker code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,21 @@
-var CACHE_NAME = "my-site-cache-v1";
+const CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-var urlsToCache = {
+const FILES_TO_CACHE = {
     //add files to cache
 }
 
 //service worker code
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
+
+//clean up old caches
+
+
+//fetch from caches
